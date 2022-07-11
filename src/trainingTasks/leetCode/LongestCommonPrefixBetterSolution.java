53cbb16e4209fb09f6f7a8b329db0f91b0cdf4c1
adam-stan <as1993@o2.pl>
2022-07-12 01:14:21 +0200

    Longest Common prefix task - better solution.

---
+++ src/trainingTasks/leetCode/LongestCommonPrefixBetterSolution.java
@@ -0,0 +1,23 @@
+package trainingTasks.leetCode;
+
+public class LongestCommonPrefixBetterSolution {
+
+    public String longestCommonPrefix(String[] strs) {
+
+        String prefix = strs[0];
+
+        for (int i = 1; i < strs.length; i++) {
+
+            while(strs[i].indexOf(prefix) != 0) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+            }
+        }
+        return prefix;
+    }
+
+    public static void main(String[] args) {
+        String[] strs = {"flor", "flowsdf", "floght"};
+        LongestCommonPrefix longestCommonPrefix = new LongestCommonPrefix();
+        System.out.println(longestCommonPrefix.longestCommonPrefix(strs));
+    }
+}
